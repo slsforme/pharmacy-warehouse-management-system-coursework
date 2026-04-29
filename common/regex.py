5c3d23450b9a20c54f1e6b2167a7c98441cef77f
slsforme <27ofdecember@gmail.com>
2026-04-29 16:57:15 +0300

    Добавил модель Drug (лекарство), пока что только с именем и fk на Manufacturer (производителя). Исправил 'blank' во всех моделях

--- common/regex.py
+++ common/regex.py
@@ -1,5 +1,3 @@
-import re
-
 SPECIAL_CHARS = r"!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?`~\s"
 
 ONLY_CYRILLIC_LATIN_SPECIAL_SYMBOLS_AND_NUMBERS_STRING_REGEX = rf'^[а-яА-ЯёЁa-zA-Z\d{SPECIAL_CHARS}]+$'
@@ -16,6 +14,16 @@ ONLY_NUMBERS_REGEX = r'^\d+$'
 
 PHONE_NUMBER_REGEX = r'^(\+7|8)[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$'
 
+INN_REGEX = r'^\d{10}(\d{2})?$'
+
+KPP_REGEX = r'^\d{9}$'
+
+OGRN_REGEX = r'^\d{13}(\d{2})?$'
+
+OKTMO_REGEX = r'^\d{8}(\d{3})?$'
+
+LICENSE_REGEX = r'^ЛО-\d{2}-\d{2}-\d+$'
+
 __all__ = [
     "ONLY_CYRILLIC_LATIN_SPECIAL_SYMBOLS_AND_NUMBERS_STRING_REGEX",
     "ONLY_SYMBOLS_AND_SPECIAL_SYMBOLS_STRING_REGEX",
@@ -24,4 +32,9 @@ __all__ = [
     "ONLY_LATIN_STRING_REGEX",
     "PHONE_NUMBER_REGEX",
     "ONLY_NUMBERS_REGEX",
+    "OGRN_REGEX",
+    "OKTMO_REGEX",
+    "INN_REGEX",
+    "KPP_REGEX",
+    "LICENSE_REGEX",
 ]
